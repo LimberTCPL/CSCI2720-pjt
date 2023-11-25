0ed63595b402fc0f9b20682c22efc05a6e1185c3
SYsalomon <143250872+SYsalomon@users.noreply.github.com>
2023-11-25 21:35:36 +0800

    Add files via upload

---
+++ salomon/src/server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json({ type: ["application/json", "application/csp-report"] }));
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/project');
+
+const db = mongoose.connection;
+// Upon connection failure
+db.on('error', console.error.bind(console, 'Connection error:'));
+// Upon opening the database successfully
+db.once('open', function () {
+  console.log("Connection is open...");
+
+  const locationSchema = mongoose.Schema({
+    locationID: Number,
+    location: String,
+    latitude: Number,
+    longitude: Number,
+  });
+  
+  const location = mongoose.model('venues', locationSchema)
+
+  location.find({})
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log("failed to read");
+  }); 
+
+
+  app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
+
+  //for the search of location feature of the search bar
+  app.post('/locations', (req, res) => {
+    function setUpSearch (keywords){//setting up the RegExp for finding the results
+      let tempStr = "" 
+      keywords.forEach(keyword => {tempStr += "(?=.*" + keyword + ")"});
+      let searchRegex =  tempStr 
+      //console.log(searchRegex)
+      return searchRegex;
+    } 
+
+      const target = req.body.name //this retrieves the contents in the search bar
+      console.log(target)
+      let searchKeywords = ''
+      if (target){ //handles the case where the search box is non-empty => modify searchKeywords. Hence if the search box is empty searchKeywords = ''
+          let keywords = target.trim().split(/[\s_()]+/)
+          searchKeywords = RegExp(setUpSearch(keywords),'i');
+          //console.log(searchKeywords)
+      }
+
+      let response = [];
+      location.find({ location: {$regex: searchKeywords}})
+      .then((data) => {
+        response = data;
+        res.send(response); 
+      }); 
+    });
+  });
+
+const server = app.listen(5000);
