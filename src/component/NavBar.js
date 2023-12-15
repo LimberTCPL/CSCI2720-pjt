c7135e20559ec06307be2ea5c5895e919a7a8f83
SYsalomon <salomonyeung123@gmail.com>
2023-12-15 23:14:37 +0800

    a

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -52,9 +52,9 @@ class NavBar extends Component {
               <Link class="nav-link" to="/map">Map</Link>
               <Link class="nav-link" to="/locations">Locations</Link>
               <Link class="nav-link" to="/events">Events</Link>
-              <div style = {{ position : "absolute", top: 0, right: '60pt', maxWidth: '50vw'}}>
+              <div style = {{ position : "absolute", top: 0, right: '60pt', maxWidth: '50vw' }}>
                 <div>Welcome Back! {username} </div>
-                <button onClick = { handleLogout }>Logout</button>
+                <button onClick = { handleLogout } style={{backgroundColor: '#e89c3f'}}>Logout</button>
           </div>
               {/*<Link class="nav-link" to="/search">Search</Link>*/}
               </div>
